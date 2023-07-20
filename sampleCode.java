9c52ca91b1e84a59188d16106164376653ff1f4d
prathaprajd <prathapraj.4ever@gmail.com>
2023-07-20 13:59:45 +0530

    Update sampleCode.java

--- sampleCode.java
+++ sampleCode.java
@@ -2,5 +2,6 @@ public class sampleCode{
     public static void main(String[] args){
         System.out.println("printing java program");
         System.out.println("trigger from web hook");
+        System.out.println("second change");
     }
 }
